d42c7f2dc51b5807cfdb044e19ddb9f35122cc5d
Yankun Meng <yankun.meng25@gmail.com>
2024-01-31 20:43:07 -0500

    init

---
+++ Frontend/src/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+    return (
+        <div className="NavBar">
+            <div className="nav-logo">
+
+            </div>
+            <ul className="nav-menu">
+
+            </ul>
+
+
+        </div>
+    )
+}
